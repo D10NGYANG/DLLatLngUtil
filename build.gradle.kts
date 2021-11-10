ad9d7770197128cf33ff832a91c55dd6325fc82f
D10NG <D10NGYANG@163.com>
2021-11-10 09:59:06 +0800

    将Android项目改成JVM项目

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "1.5.31"
+    id("maven")
+    id("maven-publish")
+}
+
+group = "com.d10ng"
+
+repositories {
+    mavenCentral()
+    maven("https://jitpack.io" )
+}
+
+kotlin {
+    java {
+        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
