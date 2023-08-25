ce6ae7726944ac0e8127485c2dc555085614ec93
D10NGYANG <D10NGYANG@163.com>
2023-08-25 15:22:04 +0800

    feat: update kotlin 1.9.10, gradle 8.3；

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ val bds100MavenPassword: String by project
 val npmJsToken: String by project
 
 plugins {
-    kotlin("multiplatform") version "1.9.0"
+    kotlin("multiplatform") version "1.9.10"
     id("maven-publish")
     id("dev.petuska.npm.publish") version "3.4.1"
     id("com.github.ben-manes.versions") version "0.47.0"
@@ -87,4 +87,9 @@ tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
     rejectVersionIf {
         isNonStable(candidate.version)
     }
+}
+
+// TODO 修复gradle 8.0以后出现任务依赖不声明导致的问题，待后续修复了再移除
+tasks.named("jsNodeProductionLibraryPrepare") {
+    dependsOn("jsProductionExecutableCompileSync")
 }
